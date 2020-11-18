cb8bb70e0ff18fb61dbd17c35a1096fb93ddff1c
Veremeichik Nikita <lol-by@rambler.ru>
2020-11-18 20:45:39 +0300

    Start Spring Boot application #1

---
+++ src/main/java/com/nc/example/CatsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsSpringBootApplication.class, args);
+	}
+
+}
